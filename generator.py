16f9d498efb9be24340daba65ac4f1e5b823e774
Alex Myasoedov <msoedov@gmail.com>
2018-03-11 14:06:46 -0400

    Generated boilerplate

--- generator.py
+++ generator.py
@@ -3,15 +3,27 @@ from jinja2 import Template
 # >> > s = "{% for element in elements %}{{loop.index}} {% endfor %}"
 # >> > Template(s).render(elements=["a", "b", "c", "d"])
 
-headers = """
-def Var(name): return name
-"""
+headers = Template("""
+{% for t in spec %}{{t.name}}{% if not loop.last %} = {% endif %}{% endfor %} = None
 
+""")
+
+# Todo: topological sort
 
 types_decl = Template("""
 class {{name}}(object):
+    \"""[summary]
+    \"""
 {% for f in fields %}
-    {{ f['name'] }} = Var({{ f['etype'] }})
+    {{ f['name'] }}:{{ f['etype'] }} = {{f.defaultValue or None}}
+{% endfor %}
+{% for f in input_fields %}
+    \"""{{f.description}}\"""
+    {{ f['name'] }}:{{ f['etype'] }} = {{f.defaultValue}}
+{% endfor %}
+{% for f in enums %}
+    \"""{{f.description}}\"""
+    {{ f.name }} = "{{f.name}}"
 {% endfor %}
 
 {% for m in methods %}
@@ -23,14 +35,31 @@ class {{name}}(object):
 """)
 
 
+def know_types(t):
+    mapping = {
+        'String': 'str',
+        'Boolean': 'bool',
+        'Intger': 'int',
+    }
+    return mapping.get(t, t)
+
+
 def guess_type(ts):
+    return know_types(of_type(ts))
+
+
+def of_type(ts):
     """
-    >>> {'kind': 'OBJECT', 'name': 'User', 'ofType': None}
-    User
-    >>> {'kind': 'NON_NULL', 'name': None, 'ofType': {'kind': 'SCALAR', 'name': 'String', 'ofType': None}}
-    String
+    >>> of_type({'kind': 'OBJECT', 'name': 'User', 'ofType': None})
+    'User'
+    >>> of_type({'kind': 'NON_NULL', 'name': None, 'ofType': {'kind': 'SCALAR', 'name': 'String', 'ofType': None}})
+    'String'
+    >>> of_type({"kind": "ENUM", "name": "ProjectState"})
+    'ProjectState'
     """
     if ts.get('kind') == 'OBJECT':
+        return ts['name']
+    if ts.get('kind') == 'ENUM':
         return ts['name']
     if ts.get('args'):
         return"Method"
@@ -38,10 +67,10 @@ def guess_type(ts):
         return ts['name']
     subKind = ts.get('type') or ts.get('ofType')
     if not subKind:
-        return "None?"
+        return "map"
         # raise ValueError(ts)
     if subKind['kind'] == 'LIST':
-        subT = guess_type(ts.get('type') or ts.get('ofType'))
+        subT = of_type(ts.get('type') or ts.get('ofType'))
         return'[{}]'.format(subT)
     if subKind['name']:
         return subKind['name']
@@ -66,6 +95,31 @@ def Fields(t):
     return [f for f in fields if f.get('args', []) == []]
 
 
+def InputFields(t):
+    """
+    "name": "clientMutationId",
+    "description": "A unique identifier for the client performing the mutation.",
+    "type": {
+        "kind": "SCALAR",
+        "name": "String",
+        "ofType": null
+    },
+    "defaultValue": null
+    """
+
+    fields = t.get('inputFields')
+    if not fields:
+        return []
+    for f in fields:
+        f['etype'] = guess_type(f)
+    return fields
+
+
+def EnumValues(t):
+    fields = t.get('enumValues') or []
+    return fields
+
+
 def Methods(t):
     fields = t['fields'] or []
     methods = [methodF(f) for f in fields if f.get('args')]
@@ -73,11 +127,14 @@ def Methods(t):
 
 
 def show(spec):
+    print(headers.render(spec=spec['types']))
     for thing in spec['types']:
         if thing['name'].startswith('__'):
             continue
         print(types_decl.render(
             name=thing['name'],
             fields=Fields(thing),
-            methods=Methods(thing)),
-        )
+            methods=Methods(thing),
+            input_fields=InputFields(thing),
+            enums=EnumValues(thing),
+        ))
